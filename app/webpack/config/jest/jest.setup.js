50d2608551b12e879a6b34a40af4f9ac72d87e96
James Sear <jpsear@gmail.com>
2018-01-03 16:36:20 +0000

    Adds jest

---
+++ app/webpack/config/jest/jest.setup.js
@@ -0,0 +1,7 @@
+global.expect = require('chai').expect
+global.enzyme = require('enzyme')
+
+// enzyme globals for simplicity
+global.mount = enzyme.mount
+global.shallow = enzyme.shallow
+global.render = enzyme.render
