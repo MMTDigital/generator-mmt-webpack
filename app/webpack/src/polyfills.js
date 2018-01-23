f1621b62f91b837fb8509787e1c4eae29587f5e3
James Sear <jpsear@gmail.com>
2018-01-23 15:41:59 +0000

    Adds libs for async/await

--- app/webpack/src/polyfills.js
+++ app/webpack/src/polyfills.js
@@ -4,9 +4,14 @@
  * If you need to support older browsers that don't have certain
  * JavaScript functionality yet, you may need to polyfill the
  * feature. To do this, just import the specific function from
- * the core-js library. Resist the temptation to include the
- * whole thing, as it can add a lot of weight to your bundle.
- * https://github.com/zloirock/core-js
+ * the core-js library (or an external polyfill, if you're sure
+ * you need it. Resist the temptation to include the whole of core-js,
+ * as it can add a lot of weight to your bundle.
+ * core-js: https://github.com/zloirock/core-js
  */
 
- import 'core-js/fn/array/for-each'
+// ForEach
+import 'core-js/fn/array/for-each'
+
+// async/await support
+import 'regenerator-runtime/runtime'
