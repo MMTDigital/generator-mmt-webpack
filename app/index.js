3cf6d04ea08e4371594410328f46fd3f1f9ccb70
Owen Ayres <owen.ayres@gmail.com>
2018-04-20 11:03:03 +0100

    Adds update notifier

--- app/index.js
+++ app/index.js
@@ -1,3 +1,5 @@
+require('./notifier')
+
 const Generator = require('yeoman-generator')
 const chalk = require('chalk')
 const yosay = require('yosay')
---
+++ app/notifier.js
@@ -0,0 +1,3 @@
+const updateNotifier = require('update-notifier')
+const pkg = require('../package.json')
+updateNotifier({pkg}).notify()
